1ce447905b6c09987787cb52e60116ed1bca1c75
Lucas Rodriguez <lucas@Lucass-MacBook-Pro.local>
2022-10-08 16:56:49 -0500

    Remove container action, rename some stuff, remove unused github package, update README, add logic for debugging

---
+++ README.md
@@ -0,0 +1,7 @@
+# github-javascript-actions
+
+A repository for experimenting with and creating JavaScript GitHub actions.
+
+## Development
+
+When changes are made to the javascript source code, be sure to run `npm run build` to recompile the code into the dist/index.js file. This will execute the "build" script found in the `package.json` file in the root of the repository.
---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+const exec = require('@actions/exec')
+const io = require('@actions/io')
+const { getZarfBinary } = require('./lib/utils');
+
+async function setup() {
+  try {
+    // Get version of zarf to be installed
+    const version = core.getInput('version');
+
+    // Download the specific version of zarf
+    const download = getZarfBinary(version);
+    const pathToBinary = await tc.downloadTool(download.url);
+
+    // Expose the zarf binary by adding it to the PATH
+    core.addPath(pathToBinary);
+
+    // Get the path to the zarf binary
+    const zarfPath = await io.which('zarf', true);
+
+    // Execute the zarf binary
+    await exec.exec(`"${zarfPath}"`);
+
+  } catch (err) {
+    core.setFailed(err);
+  }
+}
+
+module.exports = setup
