3951f9b0fb860933f463dec2bd510747c6095fcf
Lucas Rodriguez <lucas@Lucass-MacBook-Pro.local>
2022-10-13 12:50:37 -0500

    Testing windows runner

--- dist/index.js
+++ dist/index.js
@@ -23,7 +23,8 @@ function mapArch(arch) {
 function mapOS(os) {
   const mappings = {
     darwin: "Darwin",
-    linux: "Linux"
+    linux: "Linux",
+    win32: "Windows"
   };
   return mappings[os] || os;
 }
@@ -34,8 +35,25 @@ function getZarf(version) {
   const filename = `zarf_v${ version }_${ mapOS(platform) }_${ mapArch(arch) }`;
   const url = `https://github.com/defenseunicorns/zarf/releases/download/v${ version }/${ filename }`;
   return {
+    filename,
     url
   };
+}
+
+// Append .exe file extension to zarf binary if running on a Windows runner
+let zarfWindowsDownloadURL;
+let windowsBinPath;
+
+function isWindowsRunner(version) {
+    if (os.platform().startsWith("win")) {
+      const fileExtension = ".exe";
+      const windowsExecutable = getZarf(version).filename + fileExtension;
+      zarfWindowsDownloadURL = `https://github.com/defenseunicorns/zarf/releases/download/v${ version }/${ windowsExecutable }`;
+    }
+    return {
+      zarfWindowsDownloadURL,
+      windowsBinPath
+    };
 }
 
 async function setupZarf() {
@@ -43,31 +61,49 @@ async function setupZarf() {
     // Get version of zarf from user input
     const version = core.getInput("version");
 
-    // Set the path where the zarf binary will be installed
-    const homeDirectory = os.homedir();
-    const binPath = ".zarf/bin/zarf";
-    const installPath = path.join(homeDirectory, binPath);
-    core.info(`Zarf version v${version} will be installed at ${installPath}`);
-
-    // Download the specified version of zarf
     const download = getZarf(version);
-    const zarfDownloadURL = download.url;
-    core.info(`Downloading the zarf binary from ${zarfDownloadURL}`);
+
+    // Set the path where the zarf binary will be installed
+    let binPath = "";
+    let zarfDownloadURL;
+
+    if (isWindowsRunner) {
+      binPath = ".zarf\\bin\\zarf.exe";
+      zarfDownloadURL = isWindowsRunner().zarfWindowsDownloadURL;
+    } else {
+      binPath = ".zarf/bin/zarf";
+      zarfDownloadURL = download.url;
+    }
+
+    const homeDirectory = os.homedir();
+    const installPath = path.join(homeDirectory, binPath);
+    core.info(`Zarf version v${ version } will be installed at ${ installPath }`);
+
+    // Download the zarf binary
+    core.info(`Downloading the zarf binary from ${ zarfDownloadURL }`);
     const pathToBinary = await tc.downloadTool(zarfDownloadURL, installPath);
-    core.info(`Successfully downloaded ${zarfDownloadURL}`);
-    core.info(`The zarf binary is at ${pathToBinary}`);
+    core.info(`Successfully downloaded ${ zarfDownloadURL }`);
+    core.info(`The zarf binary is at ${ pathToBinary }`);
 
     // Add read/write/execute permissions to the binary file
-    core.info(`Adding read/write/execute permissions to ${pathToBinary}`);
+    core.info(`Adding read/write/execute permissions to ${ pathToBinary }`);
     fs.chmodSync(pathToBinary, "700");
 
     // Cache the zarf binary
     core.info("Caching the zarf binary...");
-    const cachedPath = await tc.cacheFile(pathToBinary, "zarf", "zarf", version);
-    core.info(`Cached the zarf binary at ${cachedPath}/zarf`);
+
+    let cachedPath = "";
+
+    if (isWindowsRunner) {
+      cachedPath = await tc.cacheFile(pathToBinary, "zarf.exe", "zarf", version);
+      core.info(`Cached the zarf binary at ${ cachedPath }/zarf.exe`);
+    } else {
+      cachedPath = await tc.cacheFile(pathToBinary, "zarf", "zarf", version);
+      core.info(`Cached the zarf binary at ${ cachedPath }/zarf`);
+    }
 
     // Expose the zarf binary by adding it to the $PATH environment variable
-    core.info(`Adding ${cachedPath}/zarf to the $PATH...`);
+    core.info(`Adding ${ cachedPath }/zarf to the $PATH...`);
     core.addPath(cachedPath);
     
     // Zarf is ready for use
