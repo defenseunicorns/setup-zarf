b477e867d0db06176251a5a6df8b1ae8f5398ccc
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2022-11-02 04:05:40 -0500

    Remove unused package

--- src/setup-zarf.ts
+++ src/setup-zarf.ts
@@ -4,7 +4,6 @@ import * as tc from "@actions/tool-cache";
 import { chmodSync } from "fs";
 import * as os from "os";
 import { join } from "path";
-import { stringify } from "querystring";
 
 let platform: string = ""
 
@@ -42,49 +41,49 @@ export function mapOS() {
 }
 
 export async function setupZarf(runnerPlatform: string) {
-  try {
-    // Get user input
-    const version: string = core.getInput("version");
-    const downloadInitPackage: boolean = core.getBooleanInput("download-init-package");
-    
-    // Download init package
-    if (downloadInitPackage === true) {
-      const tarball: string = `zarf-init-${ mapArch() }-${ version }.tar.zst`;
-      const initPackagePath: string = join(os.homedir(), ".zarf", tarball);
-      const initPackageURL: string = `https://github.com/defenseunicorns/zarf/releases/download/${ version }/${ tarball }`;
-      const pathToInitPackage: string = await tc.downloadTool(initPackageURL, initPackagePath);
-      await cp(pathToInitPackage, process.cwd());
-    }
-
-    // Download zarf binary
-    const exePrefix: string = `zarf_${ version }_`
-    const exeSuffix: string = os.platform().startsWith("win") ? ".exe" : "";
-    const binary: string = exePrefix + runnerPlatform + exeSuffix;
-    const binaryURL: string = `https://github.com/defenseunicorns/zarf/releases/download/${ version }/${ binary }`;
-    const binPath: string = os.platform().startsWith("win") ? ".zarf\\bin\\zarf.exe" : ".zarf/bin/zarf";
-    const installPath: string = join(os.homedir(), binPath);
-    const zarfBinary: string = await tc.downloadTool(binaryURL, installPath);
-
-    // Add read/write/execute permissions to binary
-    chmodSync(zarfBinary, "700");
-    
-    // Cache the zarf binary
-    const binaryFile: string = os.platform().startsWith("win") ? "zarf.exe" : "zarf";
-    const toolName: string = "zarf";
-    const binCachedPath: string = await tc.cacheFile(zarfBinary, binaryFile, toolName, version);
-
-    // Add binary to the $PATH
-    core.addPath(binCachedPath);
-    
-    core.info("Zarf has been successfully installed/configured and is ready to use!");
-
-    return version;
-
-  } catch(error) {
-      let errorMessage: string = "Failed to setup Zarf";
-      if (error instanceof Error) {
-        errorMessage = error.message;
+    try {
+      // Get user input
+      const version: string = core.getInput("version");
+      const downloadInitPackage: boolean = core.getBooleanInput("download-init-package");
+      
+      // Download init package
+      if (downloadInitPackage === true) {
+        const tarball: string = `zarf-init-${ mapArch() }-${ version }.tar.zst`;
+        const initPackagePath: string = join(os.homedir(), ".zarf", tarball);
+        const initPackageURL: string = `https://github.com/defenseunicorns/zarf/releases/download/${ version }/${ tarball }`;
+        const pathToInitPackage: string = await tc.downloadTool(initPackageURL, initPackagePath);
+        await cp(pathToInitPackage, process.cwd());
       }
-      core.setFailed(errorMessage);
-  }
+
+      // Download zarf binary
+      const exePrefix: string = `zarf_${ version }_`
+      const exeSuffix: string = os.platform().startsWith("win") ? ".exe" : "";
+      const binary: string = exePrefix + runnerPlatform + exeSuffix;
+      const binaryURL: string = `https://github.com/defenseunicorns/zarf/releases/download/${ version }/${ binary }`;
+      const binPath: string = os.platform().startsWith("win") ? ".zarf\\bin\\zarf.exe" : ".zarf/bin/zarf";
+      const installPath: string = join(os.homedir(), binPath);
+      const zarfBinary: string = await tc.downloadTool(binaryURL, installPath);
+
+      // Add read/write/execute permissions to binary
+      chmodSync(zarfBinary, "700");
+      
+      // Cache the zarf binary
+      const binaryFile: string = os.platform().startsWith("win") ? "zarf.exe" : "zarf";
+      const toolName: string = "zarf";
+      const binCachedPath: string = await tc.cacheFile(zarfBinary, binaryFile, toolName, version);
+
+      // Add binary to the $PATH
+      core.addPath(binCachedPath);
+      
+      core.info("Zarf has been successfully installed/configured and is ready to use!");
+
+      return version;
+
+    } catch(error) {
+        let errorMessage: string = "Failed to setup Zarf";
+        if (error instanceof Error) {
+          errorMessage = error.message;
+        }
+        core.setFailed(errorMessage);
+    }
 }
